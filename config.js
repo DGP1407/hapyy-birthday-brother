b377765402cee2ae870f608cca276c8ca958f605
DGP1407 <104746803+DGP1407@users.noreply.github.com>
2022-09-18 14:02:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const nameGirl = 'Anh Hai';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Anh Hai';
+const contentCard = 'Chúc anh hai một ngày vui vẻ và sẽ luôn vui tươi và luôn thành công trên con đường anh lựa chọn. Happy Birthday to you!';
+const giftImage = 'brother.jpg';
+const giftImageBase64 = "data:image/png;base64, " + base64;
